de56458c418ab668328cc7ad73aba00f0e84a9b3
xuan2018 <xuanfu1987@hotmail.com>
2019-02-24 14:52:12 -0500

    add HelloWorld program

---
+++ src/testGitJenkins/HelloWorld.java
@@ -0,0 +1,12 @@
+package testGitJenkins;
+
+public class HelloWorld {
+	
+	public static void main(String args[]) {
+		for (int i=0; i < 10; i++)
+		{
+		  System.out.println("Hello World: " + i);	
+		}
+	}
+
+}
